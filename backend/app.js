8e5954135157a22b3294d3b26cd9d0c3b5060045
Alladan04 <alla.danielyan.2004@mail.ru>
2023-10-29 15:37:04 +0300

    web_socket

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,61 @@
 import * as model from "./models.js";
 import express from "express";
 const app = express();
-const PORT = 3001;
+
+const port = 3001;
+import  {WebSocketServer} from 'ws';
+import http from 'http';
+const server = http.createServer(app);
+
+const webSocketServer = new WebSocketServer({ server });
+
+webSocketServer.on('connection', ws => {
+  console.log("connected");
+   ws.on('message', m => {
+    console.log("m");
+   });
+
+   ws.on("error", e => ws.send(e));
+
+   app.post('/station_charge',(req, res) => {
+    //отправить в бд
+    model.getStations()
+    .then((response)=>{
+
+      ws.send(response);
+      console.log("got info ")
+    })
+    .catch((err)=>
+      ws.send(501)
+    )
+  
+   })
+   
+   ws.send('Hi there, I am a WebSocket server');
+});
+
+
+
+
+//почитать про await, переделать в 2 строчки
+//добавить одельно обработчик ошибок в express
+server.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 app.use(express.json());
 
 
-app.get(`/station`, (request, res) => {
-  model
-    .getStations()
-    .then((response) => {
-      res.status(200).send(response);
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+app.get(`/station/:id`, (request, res) => {
+ /* model
+  .getStations()
+  .then((response) => {
+    res.status(200).send(response);
+  })
+  .catch((error) => {
+    res.status(500).send(error);
+  });*/
+  res.status(200).send("ok")
 
 });
 
